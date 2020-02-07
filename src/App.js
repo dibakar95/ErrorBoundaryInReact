04d30ab1522a5214da44e00096190a3e18c7fd6f
Dibakar mandal <dibakar.redbulls@gmail.com>
2020-02-07 17:07:51 +0530

    added New HeroName
    Heroname added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from "react";
 import "./styles.css";
-import Hero from './Components/Hero';
-import ErrorBoundary from './Components/ErrorBoundary';
+import Hero from "./Components/Hero";
+import ErrorBoundary from "./Components/ErrorBoundary";
 //import Child from './Child'
 export default function App() {
-  return <div className="App" >
-    <ErrorBoundary>
-      <Hero heroname = 'batman'/>
-      <Hero heroname = 'Superman' />
-      <Hero heroname = 'Joker'/>
-    </ErrorBoundary>
+  return (
+    <div className="App">
+      <ErrorBoundary>
+        <Hero heroname="batman" />
+        <Hero heroname="Superman" />
+        <Hero heroname="Jackyl" />
+        <Hero heroname="" />
+      </ErrorBoundary>
     </div>
+  );
 }
